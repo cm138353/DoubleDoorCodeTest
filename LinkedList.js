9d7704a5e944d44d001acecb2543180d3fecc99c
cm138353 <christian.martinez442@yahoo.com>
2019-06-09 11:50:56 -0700

    forgot to push, added incomplete linked list and added an index.html

---
+++ LinkedList.js
@@ -0,0 +1,24 @@
+class Node {
+    constructor(){
+        this.data = null;
+        this.next = null;
+    }
+    constructor(data) {
+        this.data = data;
+        this.next = null; 
+    }
+ }
+ class LinkedList {
+    
+    constructor() {
+        let root = new Node();    
+        root.next = null;
+        root.data = null;
+        this.root = root;
+    }
+
+    add(node){
+        
+    }
+ }
+
